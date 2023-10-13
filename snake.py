0e292d7e565f71887189f9a1cb3a9ac99412ff50
Connor Malley <malleyconnor@knights.ucf.edu>
2023-10-12 20:39:25 -0400

    Fix bug

--- snake.py
+++ snake.py
@@ -88,15 +88,25 @@ class SnakeGame:
             self.direction = Direction.RIGHT
 
         # Change the snakes direction
+
         def change_direction(self, direction):
-            if direction == Direction.UP and self.snake.direction != Direction.DOWN:
-                self.snake.direction = Direction.UP
-            elif direction == Direction.DOWN and self.snake.direction != Direction.UP:
-                self.snake.direction = Direction.DOWN
-            elif direction== Direction.LEFT and self.snake.direction != Direction.RIGHT:
-                self.snake.direction = Direction.LEFT
-            elif direction == Direction.RIGHT and self.snake.direction != Direction.LEFT:
-                self.snake.direction = Direction.RIGHT
+            """
+            Changes the snakes direction.
+
+            Input:
+                direction (Direction object) - Which direction to change to
+
+            Returns:
+                None
+            """
+            if direction == Direction.UP and self.direction != Direction.DOWN:
+                self.direction = Direction.UP
+            elif direction == Direction.DOWN and self.direction != Direction.UP:
+                self.direction = Direction.DOWN
+            elif direction== Direction.LEFT and self.direction != Direction.RIGHT:
+                self.direction = Direction.LEFT
+            elif direction == Direction.RIGHT and self.direction != Direction.LEFT:
+                self.direction = Direction.RIGHT
 
 
     # displaying Score function
